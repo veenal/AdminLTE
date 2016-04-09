63a79555621888cc68bcff8941857b10f722a000
veenal <23veenal.bansal@gmail.com>
2016-04-09 17:38:59 +0530

    Added files via upload

--- js/app.js
+++ js/app.js
@@ -8,3 +8,56 @@ app.directive('headerPanel', function(){
 		
 	};
 });
+<<<<<<< HEAD
+
+app.directive('sidebarPanel', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'sidebar-panel'
+
+	};
+});
+
+app.directive('csidebarPanel', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'csidebar-panel'
+
+	};
+});
+
+app.controller('EmailController',function(){
+	// body...
+	this.mail = [
+		{
+			id: "123@456.com",
+			message: "hello"
+		},
+		{
+			id : "abc@alpha.com",
+			message: "hello there how are you"
+		},
+		{
+			id: "xyz@beta.com",
+			message: "hello world"
+		}
+		];
+	});
+
+
+/*var gems = [
+	{
+		id: "123@456.com",
+		message: "hello"
+	},
+		{
+			id : "abc@alpha.com",
+			message: "hello there how are you"
+		},
+		{
+			id: "xyz@beta.com",
+			message: "hello world"
+		}
+		];*/
+=======
+>>>>>>> ddd096b4d9b596536274cd27bf988e0afd5c631f
